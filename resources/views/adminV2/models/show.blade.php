62d7eefc8b70dfb713663465c4a6313b4d2964c8
Arun kumar <arunmaurya2500@gmail.com>
2025-10-25 19:38:51 +0530

    added gallery in model profle

--- resources/views/adminV2/models/show.blade.php
+++ resources/views/adminV2/models/show.blade.php
@@ -111,7 +111,7 @@
                                         </span>
                                     </p>
                                 </div>
-                                <div class="col-md-12 mt-4 text-center">
+                                <div class="col-md-12 mt-4">
                                     <strong>Gallery:</strong>
                                     @include('adminV2.models.gallery')
                                 </div>
